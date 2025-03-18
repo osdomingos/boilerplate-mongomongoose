e741d824d70b85bdce92c5214c4dc96c3d35c4b1
Osmar Domingos <osdomingos@gmail.com>
2025-03-18 11:58:54 -0300

    Implements a Person finder by only one item of food 1

--- myApp.js
+++ myApp.js
@@ -52,7 +52,11 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food}, (err, data) => {
+    if (err) return done(err)
+    done(null, data);
+  })
+  
 };
 
 const findPersonById = (personId, done) => {
