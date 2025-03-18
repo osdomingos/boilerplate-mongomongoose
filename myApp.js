f0b24752c68de958261af032ff97f7604c09fb53
Osmar Domingos <osdomingos@gmail.com>
2025-03-18 11:01:05 -0300

    Creates personSchema 1

--- myApp.js
+++ myApp.js
@@ -3,7 +3,20 @@ mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number
+  },
+  favoriteFoods: {
+    type: [String]
+  }
+})
+
+let Person = personSchema;
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
